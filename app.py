19ecf939b158d2b8eb1dc04274bed633a3bfd023
hyeonukkang <khu@xinapse.ai>
2021-02-22 18:31:35 +0900

    add blueprint route('/test')

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 from flask import Flask
+from views import main_views
 
 
 def create_app():
     app = Flask(__name__)
-
-    @app.route('/')
-    def index():
-        return 'Hello, todo!'
+    app.register_blueprint(main_views.bp)
 
     return app
-
