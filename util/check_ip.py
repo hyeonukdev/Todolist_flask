1e7924da053e15a2d1b794af3cda3035bf4e4d7e
hyeonukkang <khu@xinapse.ai>
2021-03-24 18:24:26 +0900

    delete print

--- util/check_ip.py
+++ util/check_ip.py
@@ -4,8 +4,8 @@ def ip_check(ip):
     p = '^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$'
     res = re.search(p, ip)
     if res is not None:
-        print("valid")
+        # print("valid")
         return True
     else:
-        print("Invalid")
+        # print("Invalid")
         return False
